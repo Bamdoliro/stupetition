c29b0404d44089fae90fa4cbc25e9773a083faa8
SEOKKAMONI <sj01048260279@gmail.com>
2022-10-11 10:54:13 +0900

    ADD :: MainTabBar 세팅

---
+++ src/components/MainTabBar/MainTabBar.js
@@ -0,0 +1,10 @@
+import React from "react"
+import "./MainTabBar.css"
+
+export default function MainTabBar() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,10 +1,12 @@
 import React from "react"
 import MainHeader from "../components/MainHeader/MainHeader"
+import MainTabBar from "../components/MainTabBar/MainTabBar"
 
 export default function MainPage() {
     return (
         <div>
             <MainHeader />
+            <MainTabBar />
         </div>
     )
 }
