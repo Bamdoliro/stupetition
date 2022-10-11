94996f91e19ce440a2be601a953fb647cdffffdb
SEOKKAMONI <sj01048260279@gmail.com>
2022-10-11 13:04:48 +0900

    Petition 스타일 추가 - Progress bar 만듦 - 전체적인 틀 짬 - MainContent도 틀 짬

--- src/components/PetitionList/PetitionList.js
+++ src/components/PetitionList/PetitionList.js
@@ -1,9 +1,47 @@
-import React from "react"
+import React, { useState } from "react"
+import MessageLogo from "../../assets/message.svg"
+import ArrowLogo from "../../assets/arrow.svg"
 import "./PetitionList.css"
+import styled from 'styled-components';
 
 export default function PetitionList() {
+
+    const [count, setProgress] = useState(60)
+
     return (
-        <div>
-        </div>
+        <li className="petition-list">
+            <div className="petition-content">
+                <div className="petition-area">
+                    <div>
+                        <span className="petition-title">
+                            아침운동 없애주세요
+                        </span>
+                        <span className="petition-date">
+                            02/02 - 04/02
+                        </span>
+                    </div>
+                    <img src={ArrowLogo} className="arrow-logo" />
+                </div>
+                <div className="petition-message-area">
+                    <img src={MessageLogo} className="message-logo" />
+                    <span className="message-number">
+                        23
+                    </span>
+                </div>
+                <div class="progress-bar">
+                    <Progress width={count + "%"} />
+                </div>
+            </div>
+        </li>
     )
 }
+
+const Progress = styled.div`
+    width: ${props => props.width}; 
+    height: 15px;
+    padding: 0;
+    text-align: center;
+    background-color: #4F98FF;
+    color: #111;
+    border-radius: 18px 18px 18px 18px;
+`;
