e62c43e59401ef41b64cea1810b9fc077e9e6081
SEOKKAMONI <sj01048260279@gmail.com>
2022-10-11 11:13:35 +0900

    ADD :: MainTabBar

--- src/components/MainTabBar/MainTabBar.js
+++ src/components/MainTabBar/MainTabBar.js
@@ -1,10 +1,26 @@
-import React from "react"
+import React, { useState } from "react"
 import "./MainTabBar.css"
 
 export default function MainTabBar() {
+    const [focus, setFocus] = useState(1);
+
     return (
-        <div>
-            
-        </div>
+        <ul className="main-tab-bar">
+            <li className="main-tab-item" onClick={() => setFocus(1)}>
+                <span className={focus == 1 ? "item-focus-text" : "item-text"}>
+                    진행 중인 청원
+                </span>
+            </li>
+            <li className="main-tab-item" onClick={() => setFocus(2)}>
+                <span className={focus == 2 ? "item-focus-text" : "item-text"}>
+                    대기 중인 청원
+                </span>
+            </li>
+            <li className="main-tab-item" onClick={() => setFocus(3)}>
+                <span className={focus == 3 ? "item-focus-text" : "item-text"}>
+                    완료된 청원
+                </span>
+            </li>
+        </ul>
     )
 }
