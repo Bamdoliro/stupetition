3133abe0c056be04de959770bfa5331d2349d816
SEOKKAMONI <sj01048260279@gmail.com>
2022-10-12 20:02:51 +0900

    ADD :: LoginImgBox 추가

--- src/components/LoginFrame/LoginFrame.js
+++ src/components/LoginFrame/LoginFrame.js
@@ -1,9 +1,25 @@
 import "./LoginFrame.css"
+import styled from 'styled-components';
+import Logo from "../../assets/logo.svg"
 
 export default function LoginFrame() {
     return (
-        <div>
-            sads
+        <div className="login-frame">
+            <LoginImgBox>
+                <img src={Logo} className="login-main-logo" />
+            </LoginImgBox>
+            <div className="login-box">
+
+            </div>
         </div>
     )
 }
+
+const LoginImgBox = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+    width: 50%;
+    background-color: #C6B5D7;
+`
