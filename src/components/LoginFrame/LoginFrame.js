f35cc1ca4d06cc5e1809cb5a4ee4b8d841ccb94d
SEOKKAMONI <sj01048260279@gmail.com>
2022-10-12 22:21:28 +0900

    ADD :: LoginInput, PassWordInput 추가

--- src/components/LoginFrame/LoginFrame.js
+++ src/components/LoginFrame/LoginFrame.js
@@ -9,7 +9,15 @@ export default function LoginFrame() {
                 <img src={Logo} className="login-main-logo" />
             </LoginImgBox>
             <div className="login-box">
-
+                <div className="login-inner-box">
+                    <div className="login-inner-content"> {/* 클래스명 굉장히 맘에 안듬 */}
+                        <div className="login-title">로그인</div>
+                        <div className="login-input-area">
+                            <LoginInput type="text" placeholder="이메일을 입력해 주세요." />
+                            <PassWordInput type="text" placeholder="비밀번호를 입력해 주세요." />
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
@@ -19,7 +27,26 @@ const LoginImgBox = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
-    height: 100%;
     width: 50%;
     background-color: #C6B5D7;
+`
+
+const LoginInput = styled.input`
+    border: none;
+    outline: none;
+    height: 41px;
+    font-size: 16px;
+    width: 300px;
+    border-bottom: 2.5px solid #C6B5D7;
+    margin-bottom: 24px;
+`
+
+const PassWordInput = styled.input`
+    border: none;
+    outline: none;
+    height: 41px;
+    font-size: 16px;
+    width: 300px;
+    border-bottom: 2.5px solid #C6B5D7;
+    margin-bottom: 24px;
 `
