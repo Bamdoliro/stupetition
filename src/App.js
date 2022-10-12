36f0171c1e75160e9a1a3ac6995853ffa2ed0628
SEOKKAMONI <sj01048260279@gmail.com>
2022-10-12 16:39:29 +0900

    ADD :: LoginPage 세팅

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import LoginPage from './pages/LoginPage';
 import MainPage from './pages/MainPage';
 
 function App() {
   return (
     <div className="App">
-      <MainPage />
+      {/* <MainPage /> */}
+      <LoginPage />
     </div>
   );
 }
---
+++ src/pages/LoginPage.js
@@ -0,0 +1,7 @@
+export default function LoginPage() {
+    return (
+        <div>
+            이거슨 로그인 페이지
+        </div>
+    )
+}
