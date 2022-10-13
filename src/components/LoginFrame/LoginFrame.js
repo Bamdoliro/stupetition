49f7ad98ff549269ec7b7400ce345e020039d97e
SEOKKAMONI <sj01048260279@gmail.com>
2022-10-13 17:46:36 +0900

    ADD :: LoginButton 추가

--- src/components/LoginFrame/LoginFrame.js
+++ src/components/LoginFrame/LoginFrame.js
@@ -3,6 +3,11 @@ import styled from 'styled-components';
 import Logo from "../../assets/logo.svg"
 
 export default function LoginFrame() {
+
+    const LoginBtnClick = () => {
+        console.log("클릭");
+    }
+
     return (
         <div className="login-frame">
             <LoginImgBox>
@@ -14,8 +19,11 @@ export default function LoginFrame() {
                         <div className="login-title">로그인</div>
                         <div className="login-input-area">
                             <LoginInput type="text" placeholder="이메일을 입력해 주세요." />
-                            <PassWordInput type="text" placeholder="비밀번호를 입력해 주세요." />
+                            <PassWordInput type="password" placeholder="비밀번호를 입력해 주세요." />
                         </div>
+                        <LoginButton onClick={LoginBtnClick}>
+                            로그인하기
+                        </LoginButton>
                     </div>
                 </div>
             </div>
@@ -36,7 +44,7 @@ const LoginInput = styled.input`
     outline: none;
     height: 41px;
     font-size: 16px;
-    width: 300px;
+    width: 330px;
     border-bottom: 2.5px solid #C6B5D7;
     margin-bottom: 24px;
 `
@@ -46,7 +54,25 @@ const PassWordInput = styled.input`
     outline: none;
     height: 41px;
     font-size: 16px;
-    width: 300px;
+    width: 330px;
     border-bottom: 2.5px solid #C6B5D7;
     margin-bottom: 24px;
+`
+
+const LoginButton = styled.button`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: relative;
+    top: 100px;
+    float: right;
+    border: none;
+    border-radius: 8px;
+    padding: 18px;
+    outline: none;
+    background-color: #C6B5D7;
+    color: #fff;
+    width: 100px;
+    height: 30px;
+    cursor: pointer;
 `
