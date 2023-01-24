3958d59ea74f77df810aaae76011264ab5e8af2b
SEOKKAMONI <sj01048260279@gmail.com>
2023-01-24 13:10:29 +0900

    CHORE :: 인증을 모르고 둘다 넣어버림 ;;

--- src/lib/axios/customAxios.ts
+++ src/lib/axios/customAxios.ts
@@ -6,7 +6,6 @@ const customAxios = axios.create({
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
-    Authorization: `Bearer ${localStorage.getItem('access-token')}`,
   },
 });
 
