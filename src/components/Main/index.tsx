3e9b32c53e6aa931ecaa8042bcd0fb0e5c36a769
SEOKKAMONI <sj01048260279@gmail.com>
2023-02-08 17:32:43 +0900

    ADD :: Banner

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,18 @@
+import Banner from 'assets/banner.svg';
 import * as S from './style';
 
 const Main = () => {
-  return <S.Container>sad</S.Container>;
+  return (
+    <S.Container>
+      <S.Banner>
+        <S.BannerText>
+          학생청원,
+          <br />
+          학생들의 목소리를 듣다
+        </S.BannerText>
+      </S.Banner>
+    </S.Container>
+  );
 };
 
 export default Main;
--- src/components/Main/style.ts
+++ src/components/Main/style.ts
@@ -1,8 +1,22 @@
+import BannerImg from 'assets/banner.svg';
 import { color } from 'styles/theme';
+import * as T from 'styles/text';
 import styled from 'styled-components';
 
 export const Container = styled.div`
   width: 100vw;
   height: 100vh;
   background-color: ${color.white};
+`;
+
+export const Banner = styled.div`
+  display: flex;
+  align-items: center;
+  padding-left: 6.25rem;
+  background-image: url(${BannerImg});
+  height: 80%;
+`;
+
+export const BannerText = styled(T.D2)`
+  color: ${color.gray900};
 `;
