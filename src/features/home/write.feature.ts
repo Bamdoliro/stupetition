425c4e49975c9db1f428702b08cfe125f3dc429d
SEOKKAMONI <sj01048260279@gmail.com>
2023-02-24 18:43:27 +0900

    CHORE :: 청원게시하고 modal 닫아주기

--- src/features/home/write.feature.ts
+++ src/features/home/write.feature.ts
@@ -1,5 +1,6 @@
 import { writePetition } from 'apis/petition.api';
 import { useDialog } from 'hooks/useDialog';
+import { useModal } from 'hooks/useModal';
 import { useMutation } from 'react-query';
 import { useNavigate } from 'react-router-dom';
 import { WritePetitionType } from 'types/petition.type';
@@ -7,6 +8,7 @@ import { WritePetitionType } from 'types/petition.type';
 export const WriteFeature = (petitionData: WritePetitionType) => {
   const navigate = useNavigate();
   const { openDialog } = useDialog();
+  const { closeModal } = useModal();
   const { mutate } = useMutation(writePetition, {
     onSuccess: () => {
       alert('게시글 작성 성공 !!');
@@ -20,6 +22,7 @@ export const WriteFeature = (petitionData: WritePetitionType) => {
   // petitionWrite
   const write = () => {
     mutate(petitionData);
+    closeModal();
   };
 
   // confirm cancel
