900f371dc8c64f2d2985eec00af22e97660cbb77
SEOKKAMONI <sj01048260279@gmail.com>
2023-03-03 14:54:32 +0900

    ADD :: 삭제 confirm 추가

--- src/features/home/deletePetition.feature.ts
+++ src/features/home/deletePetition.feature.ts
@@ -1,14 +1,21 @@
 import { deletePetition } from 'apis/petition.api';
+import { useDialog } from 'hooks/useDialog';
 import { useSuccesToast } from 'hooks/useToast';
-import { useMutation } from 'react-query';
+import { useMutation, useQueryClient } from 'react-query';
+import * as KEY from 'constants/key.constant';
 import { useNavigate } from 'react-router-dom';
 
 const DeletePetitionFeature = (petitionId: number) => {
   const navigate = useNavigate();
+  const { closeDialog } = useDialog();
+  const queryClient = useQueryClient();
 
   const deleteMutate = useMutation(deletePetition, {
     onSuccess: () => {
       useSuccesToast('삭제 완료');
+      navigate('/');
+      queryClient.invalidateQueries([KEY.PETITION_LIST]);
+
       navigate('/');
     },
     onError: (err) => {
@@ -18,6 +25,7 @@ const DeletePetitionFeature = (petitionId: number) => {
 
   const deleteSubmit = () => {
     deleteMutate.mutate(petitionId);
+    closeDialog();
   };
 
   return { deleteSubmit };
