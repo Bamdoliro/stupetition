31942017a58480c6619679fe2e574520828738ea
SEOKKAMONI <sj01048260279@gmail.com>
2023-03-10 07:59:08 +0900

    ADD :: enabled 추가

--- src/features/home/myPetition.feature.ts
+++ src/features/home/myPetition.feature.ts
@@ -2,6 +2,8 @@ import { approvedPetition, wrotePetition } from 'apis/petition.api';
 import { useQuery } from 'react-query';
 import { StatusType } from 'types/petition.type';
 import * as KEY from 'constants/key.constant';
+import { useRecoilValue } from 'recoil';
+import { userState } from 'atoms/user.atom';
 
 interface MyPetitionList {
   createdAt: string;
@@ -12,19 +14,29 @@ interface MyPetitionList {
 }
 
 export const MyPetitionFeature = (status: StatusType) => {
+  const user = useRecoilValue(userState);
+
   const ApprovedPetitionFeature = () => {
     const { isLoading, isError, data } = useQuery<MyPetitionList[]>(
       [KEY.PETITION_APPROVED],
       () => approvedPetition(),
+      {
+        enabled: !!user.authority,
+      },
     );
 
     return { isLoading, data, isError };
   };
 
   const WrotePetitionFeature = () => {
+    const user = useRecoilValue(userState);
+
     const { isLoading, isError, data } = useQuery<MyPetitionList[]>(
       [KEY.PETITION_WROTE],
       () => wrotePetition(),
+      {
+        enabled: !!user.authority,
+      },
     );
 
     return { isLoading, data, isError };
