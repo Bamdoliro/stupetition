e9910dc8c763cce4d1ac4e2793a18e2284947f32
SEOKKAMONI <sj01048260279@gmail.com>
2023-04-03 10:31:07 +0900

    FIX :: MiniButton Common refactoring

--- src/types/common/button.type.ts
+++ src/types/common/button.type.ts
@@ -2,11 +2,17 @@ import { ButtonHTMLAttributes } from 'react';
 
 export type ButtonOptionType = 'FILLED' | 'UNFILLED';
 
-export type MiniButtonOptionType = 'FILLED' | 'UNFILLED';
+export type MiniButtonOptionType = 'FILLED' | 'UNFILLED' | 'SCARCE_FILLED';
 
 export interface ButtonPropsType
   extends ButtonHTMLAttributes<HTMLButtonElement> {
   padding?: string;
   width?: string;
   option: ButtonOptionType;
+}
+
+export interface MiniButtonPropsType
+  extends ButtonHTMLAttributes<HTMLButtonElement> {
+  width?: string;
+  option: MiniButtonOptionType;
 }
