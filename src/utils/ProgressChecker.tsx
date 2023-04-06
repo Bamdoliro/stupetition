1a6946ef0bb836adc4904105ceaa1b0dedfb0b1b
SEOKKAMONI <sj01048260279@gmail.com>
2023-04-06 17:34:57 +0900

    FIX :: ProgressChecker Bug

--- src/utils/ProgressChecker.tsx
+++ src/utils/ProgressChecker.tsx
@@ -2,7 +2,7 @@ import { StatusType } from 'types/petition.type';
 
 export const ProgressChecker = (status: StatusType) => {
   if (status === 'PETITION') return { color: '#66BB6A', progress: '진행' };
-  if (status === 'ANSWERED') return { color: '#66BB6A', progress: '진행' };
-  if (status === 'WAITING') return { color: '#66BB6A', progress: '진행' };
+  if (status === 'ANSWERED') return { color: '#2979FF', progress: '완료' };
+  if (status === 'WAITING') return { color: '#FFA000', progress: '대기' };
   return { color: '#66BB6A', progress: '진행' };
 };
